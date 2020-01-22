66fac8e84eae96bc03cda1e5c4ea96df32b96833
raihan <raihanuddin21@gmail.com>
2020-01-22 16:04:53 +0600

    Creating the Login repository method

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
@@ -37,14 +38,48 @@ namespace DatingApp.API.Data
 			}
 		}
 
-		public Task<User> Login(string userName, string password)
+		public async Task<User> Login(string userName, string password)
 		{
-			throw new NotImplementedException();
+			var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+
+			if (user == null)
+			{
+				return null;
+			}
+
+			if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+			{
+				return null;
+			}
+
+			return user;
 		}
 
-		public Task<bool> UserExists(string userName)
+		private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
 		{
-			throw new NotImplementedException();
+			using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+			{
+				var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+				for (int i = 0; i < computedHash.Length; i++)
+				{
+					if (computedHash[i] != passwordHash[i])
+					{
+						return false;
+					}
+				}
+
+				return true;
+			}
+		}
+
+		public async Task<bool> UserExists(string userName)
+		{
+			if (await _context.Users.AnyAsync(x => x.UserName == userName))
+			{
+				return true;
+			}
+
+			return false;
 		}
 	}
 }
