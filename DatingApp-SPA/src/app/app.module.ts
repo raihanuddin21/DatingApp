c839b815a4bf1da514040a59e71cdbdca2027603
raihan <raihanuddin21@gmail.com>
2020-01-30 20:46:14 +0600

    Creating the navigation and login form

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,11 +5,21 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
 import { from } from "rxjs";
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
