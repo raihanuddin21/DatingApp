5aa9e2c99aec807e99f829d89b171be792fe6a0c
raihan <raihanuddin21@gmail.com>
2020-02-02 19:05:02 +0600

    Template form

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
-import { from } from "rxjs";
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
@@ -15,11 +15,12 @@ import { NavComponent } from './nav/nav.component';
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
       AppComponent
    ]
 })
-export class AppModule {}
+export class AppModule { }
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+
   }
 
+  login(){
+    console.log(this.model);
+  }
 }
