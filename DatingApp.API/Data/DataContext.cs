461bb3788d56948bab951b72af2a70836d3b42d8
raihan <raihanuddin21@gmail.com>
2020-05-31 15:38:05 +0600

    .

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,11 +7,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-	public class DataContext : DbContext
-	{
-		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-		public DbSet<Value> Values { get; set; }
-		public DbSet<User> Users { get; set; }
-	}
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
 }
